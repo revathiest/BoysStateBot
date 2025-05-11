8bf4b16fefe1a2938e9f5087784b097bfa43487f
revathiest <revathiest@gmail.com>
2025-05-11 11:52:03 -0500

    updated embedbuilder nmflag emoji id

--- utils/scheduleEmbedBuilder.js
+++ utils/scheduleEmbedBuilder.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = function buildScheduleEmbed(title, description, color = 0x2ECC71) {
   return new EmbedBuilder()
-    .setTitle(`<:newmexicoflag:1370750476332564520> ${title}`)
+    .setTitle(`<:newmexicoflag:1371167532697784410> ${title}`)
     .setDescription(description)
     .setColor(color)
     .setFooter({ text: 'New Mexico Boys & Girls State' });
