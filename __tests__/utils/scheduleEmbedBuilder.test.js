41b9e04b131b3874c2244fb3ad0a27a72c0705c6
revathiest <revathiest@gmail.com>
2025-05-31 19:32:30 -0500

    Fixed scheduleEmbedBuilter test

--- __tests__/utils/scheduleEmbedBuilder.test.js
+++ __tests__/utils/scheduleEmbedBuilder.test.js
@@ -4,7 +4,7 @@ const buildScheduleEmbed = require('../../utils/scheduleEmbedBuilder');
 describe('buildScheduleEmbed', () => {
   it('builds an embed with title and description', () => {
     const embed = buildScheduleEmbed('Test Title', 'Test Body', 0x123456);
-    expect(embed.data.title).toBe('<:newmexicoflag:1370750476332564520> Test Title');
+    expect(embed.data.title).toBe('<:newmexicoflag:1371167532697784410> Test Title');
     expect(embed.data.description).toBe('Test Body');
     expect(embed.data.color).toBe(0x123456);
   });
