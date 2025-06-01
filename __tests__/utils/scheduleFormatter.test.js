34a513e0811f3e88950da6a673229455335262df
revathiest <revathiest@gmail.com>
2025-05-31 19:53:03 -0500

    test: fix scheduleFormatter test (#10)

--- __tests__/utils/scheduleFormatter.test.js
+++ __tests__/utils/scheduleFormatter.test.js
@@ -1,15 +1,9 @@
 const formatScheduleList = require('../../utils/scheduleFormatter');
 
 describe('formatScheduleList', () => {
-    const formatTime = (date) =>
-      date.toLocaleTimeString(undefined, {
-        hour: '2-digit',
-        minute: '2-digit',
-      });
-  
-  it('formats a list of events', () => {
-    const event1 = new Date('2025-06-01T08:00:00Z');
-    const event2 = new Date('2025-06-01T09:00:00Z');
+  it('formats timed events with location', () => {
+    const e1 = new Date('2025-06-01T08:00:00Z');
+    const e2 = new Date('2025-06-01T09:00:00Z');
     const events = [
       { summary: 'Opening', startTime: e1, location: 'Hall' },
       { summary: 'Reg', startTime: e2, location: 'Desk' },
