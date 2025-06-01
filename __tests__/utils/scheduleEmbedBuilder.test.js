9d0e6c90e539b9486ab8e7e5992e7b57f2bb3f64
revathiest <revathiest@gmail.com>
2025-05-31 19:42:59 -0500

    test: add utils coverage

--- __tests__/utils/scheduleEmbedBuilder.test.js
+++ __tests__/utils/scheduleEmbedBuilder.test.js
@@ -1,11 +1,15 @@
-// --- __tests__/utils/scheduleEmbedBuilder.test.js ---
 const buildScheduleEmbed = require('../../utils/scheduleEmbedBuilder');
 
 describe('buildScheduleEmbed', () => {
-  it('builds an embed with title and description', () => {
+  it('builds an embed with custom color', () => {
     const embed = buildScheduleEmbed('Test Title', 'Test Body', 0x123456);
     expect(embed.data.title).toBe('<:newmexicoflag:1371167532697784410> Test Title');
     expect(embed.data.description).toBe('Test Body');
     expect(embed.data.color).toBe(0x123456);
+  });
+
+  it('uses default color when none provided', () => {
+    const embed = buildScheduleEmbed('Title', 'Body');
+    expect(embed.data.color).toBe(0x2ECC71);
   });
 });
